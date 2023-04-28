178701672add97d1989bbd5a409415de492d5c61
Zachary Perales <aardzarks@gmail.com>
2023-04-28 07:52:07 -0400

    Update config such that there are now separate environment variables that are automatically configured.
    Closes sss072/Symfony-ToDo#5

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # Wait for the MySQL server to be ready
 #./wait-for db:3306 -t 60
 
+# Overwrite the environment variable with the docker environment variable
+mv -f .env.docker .env
+
 # Create the database
 php bin/console doctrine:database:create
 
